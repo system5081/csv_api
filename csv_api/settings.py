2c537d3f2d587265b750748c23a86e37610e05c3
system5081 <nkrexkai@gmail.com>
2023-04-07 22:19:16 +0900

    22:19

--- csv_api/settings.py
+++ csv_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from .local_settings import *
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '00^c*)p1=5#bvu(o_niy3@48oyb6nmf2j1&81bckxki8812c&*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
